3dc55969c8a5e44f0cb23d9012d1f1cfdad1165b
Bello Santalla <diego.bello.external@atos.net>
2019-05-04 18:36:51 +0200

    conexion con exito del back con el front

--- src/main/java/com/example/demo/models/controllers/InstitutoControladorRest.java
+++ src/main/java/com/example/demo/models/controllers/InstitutoControladorRest.java
@@ -3,13 +3,14 @@ package com.example.demo.models.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.dao.IInstitutoDao;
 import com.example.demo.models.entity.Instituto;
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/")
 public class InstitutoControladorRest {
